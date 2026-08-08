01d372bdc5075b94f33d01a4d11f423d0b94d078
Anna Ramezoni <anna.ramezoni@gmail.com>
2026-08-08 16:26:37 +1200

    Create character name buttons to display the corresponding image

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 // ======== Carousel ======== //
 
 const cardList = document.querySelectorAll(".carousel__card");
-const cardListArray = Array.from(cardList);
-console.log(cardListArray);
+console.log(cardList);
 const nextButton = document.getElementById("next-button");
 const prevButton = document.getElementById("prev-button");
 
 // current index with display block, others with display none
 
+function hideAllCards() {
+  cardList.forEach((cardList) => (cardList.style.display = "none"));
+}
+hideAllCards();
 let currentIndex = 0;
-
-cardListArray.forEach((cardList) => (cardList.style.display = "none"));
-cardListArray[currentIndex].style.display = "block";
+cardList[currentIndex].style.display = "block";
 //change the index when press next button - add event listener to nextButton
 nextButton.addEventListener("click", displayNextItem);
 
 //each time the button is clicked the index changes and the style display to block
 function displayNextItem() {
-  if (currentIndex === cardListArray.length - 1) {
+  if (currentIndex === cardList.length - 1) {
     currentIndex = 0;
-    cardListArray[currentIndex].style.display = "block";
-    cardListArray.at(-1).style.display = "none";
+    cardList[currentIndex].style.display = "block";
+    cardList[cardList.length - 1].style.display = "none";
   } else {
     currentIndex++;
-    cardListArray[currentIndex - 1].style.display = "none";
-    cardListArray[currentIndex].style.display = "block";
+    cardList[currentIndex - 1].style.display = "none";
+    cardList[currentIndex].style.display = "block";
   }
-  console.log(currentIndex, cardListArray.length);
+  console.log(currentIndex, cardList.length);
 }
 
 //add eventListener to prevbutton and
@@ -34,21 +35,56 @@ function displayNextItem() {
 prevButton.addEventListener("click", displayPrevItem);
 function displayPrevItem() {
   if (currentIndex === 0) {
-    currentIndex = cardListArray.length - 1;
-    cardListArray[currentIndex].style.display = "block";
-    cardListArray.at(0).style.display = "none";
+    currentIndex = cardList.length - 1;
+    cardList[currentIndex].style.display = "block";
+    cardList[0].style.display = "none";
   } else {
     currentIndex--;
-    cardListArray[currentIndex + 1].style.display = "none";
-    cardListArray[currentIndex].style.display = "block";
+    cardList[currentIndex + 1].style.display = "none";
+    cardList[currentIndex].style.display = "block";
   }
-  console.log(currentIndex, cardListArray.length);
+  console.log(currentIndex, cardList.length);
 }
 
-//
-//
-//
-//
+//each button, changes the display;
+
+const characterName = [
+  "Luffy",
+  "Zoro",
+  "Sanji",
+  "Nami",
+  "Usopp",
+  "Chopper",
+  "Nico Robin",
+  "Brook",
+  "Franky",
+  "Jinbe",
+  "Going Merry",
+  "Thousand Sunny",
+];
+
+function createButtonEl(index) {
+  const characterButtons = document.querySelector(".carousel__character-btns"); //
+  const characterButton = document.createElement("button");
+  characterButtons.appendChild(characterButton);
+  characterButton.textContent = characterName[index];
+  characterButton.addEventListener("click", () => {
+    displayCharacter(index);
+  });
+  console.log(characterButton);
+}
+
+characterName.forEach((character, index) => {
+  createButtonEl(index);
+});
+
+function displayCharacter(index) {
+  // hide all cards
+  hideAllCards();
+  // show cardList[index]
+  cardList[index].style.display = "block";
+}
+
 // const $cardFrontBack = document.querySelector(".card-front-back");
 // function handleClick() {
 //   if ($cardFrontBack.classList.contains("--active")) {
