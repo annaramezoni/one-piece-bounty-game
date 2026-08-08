7d52d4f2c0979a5bc7afcebead2aa69508270912
Anna Ramezoni <anna.ramezoni@gmail.com>
2026-08-08 12:37:09 +1200

    Create function to add and remove the card display type, next and previous button working

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
-const $cardFrontBack = document.querySelector(".card-front-back");
-function handleClick() {
-  if ($cardFrontBack.classList.contains("--active")) {
-    $cardFrontBack.classList.remove("--active");
+// ======== Carousel ======== //
+
+const cardList = document.querySelectorAll(".carousel__card");
+const cardListArray = Array.from(cardList);
+console.log(cardListArray);
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
+
+// current index with display block, others with display none
+
+let currentIndex = 0;
+
+cardListArray.forEach((cardList) => (cardList.style.display = "none"));
+cardListArray[currentIndex].style.display = "block";
+//change the index when press next button - add event listener to nextButton
+nextButton.addEventListener("click", displayNextItem);
+
+//each time the button is clicked the index changes and the style display to block
+function displayNextItem() {
+  if (currentIndex === cardListArray.length - 1) {
+    currentIndex = 0;
+    cardListArray[currentIndex].style.display = "block";
+    cardListArray.at(-1).style.display = "none";
   } else {
-    $cardFrontBack.classList.add("--active");
+    currentIndex++;
+    cardListArray[currentIndex - 1].style.display = "none";
+    cardListArray[currentIndex].style.display = "block";
   }
+  console.log(currentIndex, cardListArray.length);
 }
+
+//add eventListener to prevbutton and
+
+prevButton.addEventListener("click", displayPrevItem);
+function displayPrevItem() {
+  if (currentIndex === 0) {
+    currentIndex = cardListArray.length - 1;
+    cardListArray[currentIndex].style.display = "block";
+    cardListArray.at(0).style.display = "none";
+  } else {
+    currentIndex--;
+    cardListArray[currentIndex + 1].style.display = "none";
+    cardListArray[currentIndex].style.display = "block";
+  }
+  console.log(currentIndex, cardListArray.length);
+}
+
+//
+//
+//
+//
+// const $cardFrontBack = document.querySelector(".card-front-back");
+// function handleClick() {
+//   if ($cardFrontBack.classList.contains("--active")) {
+//     $cardFrontBack.classList.remove("--active");
+//   } else {
+//     $cardFrontBack.classList.add("--active");
+//   }
+// }
