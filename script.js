30ac31de12303f844a385074f098d3849d77df37
Anna Ramezoni <anna.ramezoni@gmail.com>
2026-08-10 11:25:35 +1200

    Refactor

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // ======== Carousel ======== //
 
 const cardList = document.querySelectorAll(".carousel__card");
-console.log(cardList);
 const nextButton = document.getElementById("next-button");
 const prevButton = document.getElementById("prev-button");
 
@@ -18,35 +17,31 @@ nextButton.addEventListener("click", displayNextItem);
 
 //each time the button is clicked the index changes and the style display to block
 function displayNextItem() {
+  hideAllCards();
   if (currentIndex === cardList.length - 1) {
     currentIndex = 0;
     cardList[currentIndex].style.display = "block";
-    cardList[cardList.length - 1].style.display = "none";
   } else {
     currentIndex++;
-    cardList[currentIndex - 1].style.display = "none";
     cardList[currentIndex].style.display = "block";
   }
-  console.log(currentIndex, cardList.length);
 }
 
 //add eventListener to prevbutton and
 
 prevButton.addEventListener("click", displayPrevItem);
 function displayPrevItem() {
+  hideAllCards();
   if (currentIndex === 0) {
     currentIndex = cardList.length - 1;
     cardList[currentIndex].style.display = "block";
-    cardList[0].style.display = "none";
   } else {
     currentIndex--;
-    cardList[currentIndex + 1].style.display = "none";
     cardList[currentIndex].style.display = "block";
   }
-  console.log(currentIndex, cardList.length);
 }
 
-//each button, changes the display;
+//each button(character name), changes the display;
 
 const characterName = [
   "Luffy",
@@ -79,17 +74,15 @@ characterName.forEach((character, index) => {
 });
 
 function displayCharacter(index) {
-  // hide all cards
   hideAllCards();
-  // show cardList[index]
   cardList[index].style.display = "block";
 }
 
-// const $cardFrontBack = document.querySelector(".card-front-back");
+// const cardFrontBack = document.querySelector(".card-front-back");
 // function handleClick() {
-//   if ($cardFrontBack.classList.contains("--active")) {
-//     $cardFrontBack.classList.remove("--active");
+//   if (cardFrontBack.classList.contains("--active")) {
+//     cardFrontBack.classList.remove("--active");
 //   } else {
-//     $cardFrontBack.classList.add("--active");
+//     cardFrontBack.classList.add("--active");
 //   }
 // }
